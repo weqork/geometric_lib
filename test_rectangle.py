abd7033e542de77c147aa619732c910c93213969
Maksim Hodko <120187366+weqork@users.noreply.github.com>
2024-11-28 22:57:54 +0500

    Update unittest for rectangle.py

--- test_rectangle.py
+++ test_rectangle.py
@@ -12,11 +12,17 @@ class RectangleTestCase(unittest.TestCase):
     def test_rectangle_area_zero(self):
         self.assertEqual(rectangle_area(5, 0), 0)
     
-    def test_rectangle_area_bad(self):
+    def test_rectangle_area_bad1(self):
         self.assertEqual(rectangle_area(-5, 10), "ValueError")
 
-    def test_rectangle_area_bad(self):
+    def test_rectangle_area_bad2(self):
         self.assertEqual(rectangle_area(5, -10), "ValueError")
+        
+    def test_rectangle_area_string1(self):
+        self.assertEqual(rectangle_area("a", 10), "ValueError")
+        
+    def test_rectangle_area_string2(self):
+        self.assertEqual(rectangle_area(5, "a"), "ValueError")
     
     def test_rectangle_perimeter_good(self):
         self.assertEqual(rectangle_perimeter(5, 10), 30)
@@ -24,8 +30,14 @@ class RectangleTestCase(unittest.TestCase):
     def test_rectangle_perimeter_zero(self):
         self.assertEqual(rectangle_perimeter(0, 0), 0)
     
-    def test_rectangle_perimeter_bad(self):
+    def test_rectangle_perimeter_bad1(self):
         self.assertEqual(rectangle_perimeter(-5, 10), "ValueError")
 
-    def test_rectangle_perimeter_bad(self):
+    def test_rectangle_perimeter_bad2(self):
         self.assertEqual(rectangle_perimeter(5, -10), "ValueError")
+        
+    def test_rectangle_perimeter_string1(self):
+        self.assertEqual(rectangle_area("a", 10), "ValueError")
+        
+    def test_rectangle_perimeter_string2(self):
+        self.assertEqual(rectangle_area(5, "a"), "ValueError")
