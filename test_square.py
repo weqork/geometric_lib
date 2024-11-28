01f4ebd00069306681b827e8fe8c3dfbaf027d51
Maksim Hodko <120187366+weqork@users.noreply.github.com>
2024-11-28 22:58:45 +0500

    Update unittest for square.py

--- test_square.py
+++ test_square.py
@@ -14,12 +14,18 @@ class SquareTestCase(unittest.TestCase):
 
     def test_square_area_bad(self):
         self.assertEqual(square_area(-5), "ValueError")
+        
+    def test_square_area_string(self):
+        self.assertEqual(square_area("a"), "ValueError") 
 
     def test_square_perimeter_good(self):
         self.assertEqual(square_perimeter(5), 20)
 
     def test_square_perimeter_bad(self):
         self.assertEqual(square_perimeter(-5), "ValueError")
+
+    def test_square_perimeter_string(self):
+        self.assertEqual(square_perimeter("a"), "ValueError")
         
     def test_square_perimeter_zero(self):
         self.assertEqual(square_perimeter(0), 0)
