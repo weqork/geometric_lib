9732738e09f512c03ae5ca78e9638bd60cfcc403
Maksim Hodko <120187366+weqork@users.noreply.github.com>
2024-11-28 23:00:10 +0500

    Update unittest for triangle.py

--- test_triangle.py
+++ test_triangle.py
@@ -12,11 +12,17 @@ class TriangleTestCase(unittest.TestCase):
     def test_triangle_area_zero(self):
         self.assertEqual(triangle_area(0, 4), 0)
     
-    def test_triangle_area_bad(self):
+    def test_triangle_area_bad1(self):
         self.assertEqual(triangle_area(-6, 4), "ValueError")
 
-    def test_triangle_area_bad(self):
+    def test_triangle_area_bad2(self):
         self.assertEqual(triangle_area(6, -4), "ValueError")
+
+    def test_triangle_area_string1(self):
+        self.assertEqual(triangle_area(6, "a"), "ValueError")
+
+    def test_triangle_area_string2(self):
+        self.assertEqual(triangle_area("a", 4), "ValueError")
     
     def test_triangle_perimeter_good(self):
         self.assertEqual(triangle_perimeter(3, 4, 5), 12)
@@ -24,11 +30,21 @@ class TriangleTestCase(unittest.TestCase):
     def test_triangle_perimeter_zero(self):
         self.assertEqual(triangle_perimeter(0, 0, 0), 0)
     
-    def test_triangle_perimeter_bad(self):
+    def test_triangle_perimeter_bad1(self):
         self.assertEqual(triangle_perimeter(-3, 4, 5), "ValueError")
 
-    def test_triangle_perimeter_bad(self):
+    def test_triangle_perimeter_bad2(self):
         self.assertEqual(triangle_perimeter(3, -4, 5), "ValueError")
 
-    def test_triangle_perimeter_bad(self):
+    def test_triangle_perimeter_bad3(self):
         self.assertEqual(triangle_perimeter(3, 4, -5), "ValueError")
+
+    def test_triangle_perimeter_string1(self):
+        self.assertEqual(triangle_perimeter("a", 4, 5), "ValueError")
+
+    def test_triangle_perimeter_string2(self):
+        self.assertEqual(triangle_perimeter(3, "a", 5), "ValueError")
+
+    def test_triangle_perimeter_string3(self):
+        self.assertEqual(triangle_perimeter(3, 4, "a"), "ValueError")
+
