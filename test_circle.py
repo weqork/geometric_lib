42372e1d8f97f09a1bbcfb9020bb4bcc88754d46
Maksim Hodko <120187366+weqork@users.noreply.github.com>
2024-11-28 22:54:25 +0500

    update unit test for circle.py

--- test_circle.py
+++ test_circle.py
@@ -15,6 +15,9 @@ class CircleTestCase(unittest.TestCase):
     
     def test_circle_area_bad(self):
         self.assertEqual(circle_area(-5), "ValueError")
+
+    def test_circle_area_string(self):
+        self.assertEqual(circle_area("a"), "ValueError")
     
     def test_circle_perimeter_good(self):
         self.assertAlmostEqual(circle_perimeter(5), 2 * math.pi * 5, places=1)
@@ -24,3 +27,6 @@ class CircleTestCase(unittest.TestCase):
     
     def test_circle_perimeter_bad(self):
         self.assertEqual(circle_perimeter(-5), "ValueError")
+        
+    def test_circle_perimeter_string(self):
+        self.assertEqual(circle_perimeter("a"), "ValueError")
